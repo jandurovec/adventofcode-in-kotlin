08f241d1576f3b2c61a169a9bfd2ffd25dd72cef
Jan Durovec <jandurovec@users.noreply.github.com>
2021-12-07 14:17:38 +0100

    Add Day 7
    Today morning I'm in a hurry because I have an appointment to make. I
    open the task on mobile to check if I have a chance of making it or I'll
    need to delay it to the afternoon.
    
    At first glance it reminds me of a linear regression, only in 1D. Too
    bad I don't remember anymore how to do it mathematically. I check the
    value ranges and I decide that the range is small enough for the brute
    force approach to work. This time I'm lucky that the crabs aren't spread
    across the whole long range. I wonder how embarassed I'll be when I come
    across some simple solution using math properly.
    
    As it turns out, the only modification needed for part 2 is the fuel
    consumption function. In this case I still remember how to calcualte a
    sum of arithmetic sequence so I save an extra loop using s simple
    formula and successfully submit the results.
    
    In the afternoon I just refactor the utils because I'm repeating same
    patterns when parsing the input too many times, I extract the common
    crab calculation logic to a separate function to get rid of copy-paste
    code and today's solution is ready to be committed.

---
+++ src/Day07.kt
@@ -0,0 +1,20 @@
+import kotlin.math.absoluteValue
+
+fun main() {
+    fun parseInput(name: String) = readInput(name).first().toIntList()
+
+    fun solve(crabs: List<Int>, fuel: (Int) -> Int) =
+        (crabs.minOf { it }..crabs.maxOf { it }).minOf { pos -> crabs.sumOf { fuel((it - pos).absoluteValue) } }
+
+    fun part1(crabs: List<Int>) = solve(crabs) { it }
+
+    fun part2(crabs: List<Int>) = solve(crabs) { it * (it + 1) / 2 }
+
+    val testInput = parseInput("Day07_test")
+    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
+
+    val input = parseInput("Day07")
+    println(part1(input))
+    println(part2(input))
+}
