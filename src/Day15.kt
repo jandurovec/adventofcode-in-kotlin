74b3e17c2e2efb8969d4b682e2fbb3a2ff557479
Jan Durovec <jandurovec@users.noreply.github.com>
2021-12-15 10:46:11 +0100

    Add Day 15
    Today looks fine. Dijktstra's shortest path algorithm is notoriously
    known so hopefully there will be no surprises.
    
    I start by reusing the input parsing from day 9. As expected,
    pathfinding implementation is smooth, so I submit part 1 with no issues.
    
    Part 2 seems to be just a slightly bigger grid so I simply replicate it
    (it still fits into memory) and apply the same algorithm.
    
    The result is OK and I earn the 2nd star, however, there is a noticable
    lag when calculating the result for part 2 (approx. 2s).
    
    I decide that I should probably try to optimize it to run faster.
    
    I suspected my "smart/lazy" grid implementation as a map of points to be
    the source of the issue, so I decide to revert to pure int arrays. Since
    I don't really like low-level operations to be exposed, I encapsulate
    grid implementation and the array-manipulating logic to a SquareGrid
    class.
    
    It seems that it really has helped, but I'm still constructing the big
    grid explicitly. Since I already have the logic encapsulated, I decide
    that I can calculate the risk data on-the-fly when accessing an index
    without the need to store all the values in a massive array.
    
    However, I'm facing some issues when trying to anonymously extend my
    class and override the method. The compiler isn't really helpful because
    it just says "too many arguments for public constructor". It seems that
    the syntactic sugar of moving last lambda out of function calls really
    has some disadvantages. After considerable time spent Googling I come
    across the "object : Class" construct which finally removes the compiler
    error.
    
    Now I'm happy with both the result, the code and the execution time so
    there's nothing stoping me from committing it to the repository.

---
+++ src/Day15.kt
@@ -0,0 +1,50 @@
+import java.util.*
+
+fun main() {
+    open class SquareGrid(val size: Int, val data: Array<IntArray>) {
+        open fun risk(row: Int, col: Int) = data[row][col]
+        fun neighbors(row: Int, col: Int) =
+            listOf(0 to 1, 1 to 0, 0 to -1, -1 to 0).map { row + it.first to col + it.second }
+                .filter { it.first in 0 until size && it.second in 0 until size }
+    }
+
+    fun parseInput(name: String): SquareGrid {
+        val lines = readInput(name)
+        val data = lines.map { r -> r.toCharArray().map { it.digitToInt() }.toIntArray() }.toTypedArray()
+        return SquareGrid(lines.size, data)
+    }
+
+    fun part1(grid: SquareGrid): Int {
+        val risk = Array(grid.size) { IntArray(grid.size) { Int.MAX_VALUE } }
+        risk[0][0] = 0
+        val toExplore =
+            PriorityQueue { a: Pair<Pair<Int, Int>, Int>, b: Pair<Pair<Int, Int>, Int> -> a.second - b.second }
+        toExplore.add((0 to 0) to 0)
+        while (toExplore.isNotEmpty()) {
+            val (pos, curVal) = toExplore.poll()
+            grid.neighbors(pos.first, pos.second).forEach { n ->
+                val newRisk = curVal + grid.risk(n.first, n.second)
+                if (newRisk < risk[n.first][n.second]) {
+                    risk[n.first][n.second] = newRisk
+                    toExplore.offer(n to newRisk)
+                }
+            }
+        }
+        return risk[grid.size - 1][grid.size - 1]
+    }
+
+    fun part2(grid: SquareGrid): Int {
+        return part1(object : SquareGrid(5 * grid.size, grid.data) {
+            override fun risk(row: Int, col: Int) =
+                1 + (super.risk(row % grid.size, col % grid.size) + row / grid.size + col / grid.size - 1) % 9
+        })
+    }
+
+    val testInput = parseInput("Day15_test")
+    check(part1(testInput) == 40)
+    check(part2(testInput) == 315)
+
+    val input = parseInput("Day15")
+    println(part1(input))
+    println(part2(input))
+}
