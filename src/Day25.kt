d34210454c235a383e49c2166216e341e3192e9d
Jan Durovec <jandurovec@users.noreply.github.com>
2021-12-25 08:32:29 +0100

    Add Day 25
    The last part of this year's Advent of Code is here. I start reading the
    description and I start suspecting that I'll need to find a step where
    an empty area of a particular size will open.
    
    Luckily, the task is only to wait until the cucumbers stop moving so a
    simple boolean indicator should do the trick.
    
    Since I already own the other 48 stars, there's nothing to do in part 2.
    
    I'm happy that I managed to finish the AoC this year and learned Kotlin
    in the process. Some of my predictions (e.g. about reusing the computer
    from some of the previous day) have were incorrect, however, I'm not
    particularly sad about it. If I could choose, the fewer "assembly
    language" tasks the better (even though the one from yesterday turned up
    to be quite interesting from programming point of view in the end).

---
+++ src/Day25.kt
@@ -0,0 +1,47 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var seafloor = input.map { it.toCharArray() }.toTypedArray()
+        val width = seafloor[0].size
+        val height = seafloor.size
+        var moved = true
+        var steps = 0
+        while (moved) {
+            moved = false
+            val newSeafloor = Array(height) { CharArray(width) { '.' } }
+            seafloor.forEachIndexed { y, row ->
+                row.forEachIndexed { x, c ->
+                    val nextX = (x + 1) % width
+                    when (c) {
+                        '>' -> {
+                            if (seafloor[y][nextX] == '.') {
+                                newSeafloor[y][nextX] = c
+                                moved = true
+                            } else {
+                                newSeafloor[y][x] = c
+                            }
+                        }
+                        'v' -> {
+                            val nextY = (y + 1) % height
+                            val prevX = (x + width - 1) % width
+                            if (seafloor[nextY][x] == '.' && seafloor[nextY][prevX] != '>' || (seafloor[nextY][x] == '>' && seafloor[nextY][nextX] == '.')) {
+                                newSeafloor[nextY][x] = c
+                                moved = true
+                            } else {
+                                newSeafloor[y][x] = c
+                            }
+                        }
+                    }
+                }
+            }
+            seafloor = newSeafloor
+            steps++
+        }
+        return steps
+    }
+
+    val testInput = readInput("Day25_test")
+    check(part1(testInput) == 58)
+
+    val input = readInput("Day25")
+    println(part1(input))
+}
