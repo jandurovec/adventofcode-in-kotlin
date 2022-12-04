db9766aa39c41987ac81d752913bc18d17601b5f
Jan Durovec <jandurovec@users.noreply.github.com>
2022-12-04 06:55:16 +0100

    2022 Day 04
    Today is all about interval inersections. Luckily it's still an early
    day so the Elves just use one-dimensional intervals. Not that
    more-dimensional interval intersection would be more complicated to
    apply the same algorithm in all dimensions.

---
+++ src/aoc2022/Day04.kt
@@ -0,0 +1,28 @@
+package aoc2022
+
+import readInput
+
+fun main() {
+    fun checkInterval(input: List<String>, p: (List<List<Int>>) -> Boolean) =
+        input.count { l ->
+            l.split(',')
+                .map { rng -> rng.split("-").map { it.toInt() } }
+                .let(p)
+        }
+
+    fun part1(input: List<String>) = checkInterval(input) { (a, b) ->
+        a[0] <= b[0] && a[1] >= b[1] || a[0] >= b[0] && a[1] <= b[1]
+    }
+
+    fun part2(input: List<String>) = checkInterval(input) { (a, b) ->
+        b[1] >= a[0] && b[0] <= a[1]
+    }
+
+    val testInput = readInput("aoc2022/Day04_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
+
+    val input = readInput("aoc2022/Day04")
+    println(part1(input))
+    println(part2(input))
+}
