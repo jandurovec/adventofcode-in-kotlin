9bb684bfb67a01d4ffab32aff14d7d8ee6497b38
Jan Durovec <jandurovec@users.noreply.github.com>
2023-01-15 17:50:01 +0100

    Refactor Grid
    Allow Grid to store values at individual positions, not just presence
    indicators.

--- src/Grid.kt
+++ src/Grid.kt
@@ -4,6 +4,14 @@ data class Point(val x: Int, val y: Int) {
             if (x != 0 || y != 0) x to y else null
         }
     }.map { (dx, dy) -> Point(this.x + dx, this.y + dy) }
+
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+    operator fun times(multiplier: Int) = Point(multiplier * x, multiplier * y)
 }
 
-typealias Grid = LinkedHashSet<Point>
+operator fun Int.times(point: Point) = point * this
+
+typealias Grid<T> = MutableMap<Point, T>
+
+fun <T> Grid(): Grid<T> = mutableMapOf()
+fun <T> Grid(other: Map<Point, T>): Grid<T> = other.toMutableMap()
--- src/aoc2015/Day18.kt
+++ src/aoc2015/Day18.kt
@@ -5,33 +5,34 @@ import Grid
 import Point
 import TestCase
 
-class Day18 : AdventDay<Grid, Int, Int>(2015, 18) {
+class Day18 : AdventDay<Grid<Boolean>, Int, Int>(2015, 18) {
 
     private class TestArg(val size: Int, val steps: Int)
 
-    override fun parseInput(stringInput: List<String>) = Grid().apply {
+    override fun parseInput(stringInput: List<String>) = Grid<Boolean>().apply {
         stringInput.reversed().forEachIndexed { y, row ->
             row.forEachIndexed { x, c ->
                 if (c == '#') {
-                    add(Point(x, y))
+                    this[Point(x, y)] = true
                 }
             }
         }
     }
 
-    private fun conway(input: Grid, size: Int, steps: Int, fixed: Set<Point> = emptySet()): Int {
-        var grid = Grid(input.plus(fixed))
+    private fun conway(input: Grid<Boolean>, size: Int, steps: Int, fixed: Map<Point, Boolean> = emptyMap()): Int {
+        var grid = Grid<Boolean>(input.plus(fixed))
         repeat(steps) {
-            val newGrid = Grid(fixed)
-            grid.plus(grid.flatMap { it.neighbors() }).toSet().filter { it.x in 0 until size && it.y in 0 until size }
+            val newGrid = Grid<Boolean>(fixed)
+            grid.keys.plus(grid.keys.flatMap { it.neighbors() }).toSet()
+                .filter { it.x in 0 until size && it.y in 0 until size }
                 .forEach { p ->
                     val litNeighbors = p.neighbors().count { grid.contains(it) }
                     if (grid.contains(p)) {
                         if (litNeighbors == 2 || litNeighbors == 3) {
-                            newGrid.add(p)
+                            newGrid[p] = true
                         }
                     } else if (litNeighbors == 3) {
-                        newGrid.add(p)
+                        newGrid[p] = true
                     }
                 }
             grid = newGrid
@@ -39,15 +40,15 @@ class Day18 : AdventDay<Grid, Int, Int>(2015, 18) {
         return grid.size
     }
 
-    override fun part1(input: Grid, testArg: Any?) =
+    override fun part1(input: Grid<Boolean>, testArg: Any?) =
         conway(input, if (testArg is TestArg) testArg.size else 100, if (testArg is TestArg) testArg.steps else 100)
 
-    override fun part2(input: Grid, testArg: Any?) = (if (testArg is TestArg) testArg.size else 100).let {
+    override fun part2(input: Grid<Boolean>, testArg: Any?) = (if (testArg is TestArg) testArg.size else 100).let {
         conway(
             input,
             it,
             if (testArg is TestArg) testArg.steps else 100,
-            setOf(Point(0, 0), Point(0, it - 1), Point(it - 1, 0), Point(it - 1, it - 1))
+            setOf(Point(0, 0), Point(0, it - 1), Point(it - 1, 0), Point(it - 1, it - 1)).associateWith { true }
         )
     }
 
